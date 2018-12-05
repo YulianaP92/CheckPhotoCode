73c7a0e105a050e7e4b063d5e5f7d9d65983955e
yulia <polushka.yulia@gmail.com>
2018-12-05 22:02:42 +0300

    B1-P4/4*. СheckPhotoCode.

---
+++ CheckPhotoCode/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+namespace CheckPhotoCode
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string photo;//наличие фото
+            int month;//срок фото
+            string pastePhoto;//вклеено ли фото
+            string passport;//использовалось ли фото на паспорт
+
+            Console.WriteLine("Среди документов есть фото? (да/нет)");
+            photo = Console.ReadLine();
+
+            if (photo == "да")
+            {
+                Console.WriteLine("Какое количество месяцев назад сделано фото?");
+                month = Convert.ToInt32(Console.ReadLine());
+                if (month > 3)
+                {
+                    Console.WriteLine("Отправить подающего в фотокомнату");
+                }
+                else
+                {
+                    Console.WriteLine("Фото использовалось на паспорт? (да/нет)");
+                    passport = Console.ReadLine();
+                    if (passport == "да")
+                    {
+                        Console.WriteLine("Отправить подающего в фотокомнату");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Фото вклеено? (да/нет)");
+                        pastePhoto = Console.ReadLine();
+                        if (pastePhoto == "да")
+                        {
+                            Console.WriteLine("Отправить на оплату");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Попросить вклеить фото");
+                            Console.WriteLine("Отправить на оплату");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Отправить подающего в фотокомнату");
+            }
+            Console.ReadKey();
+        }       
+    }
+}
+
